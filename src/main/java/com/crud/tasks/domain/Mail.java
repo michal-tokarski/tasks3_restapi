deb48ca4bce2306005ff115966b7fc1157e934db
michal-tokarski <michal_tokarski@wp.pl>
2020-10-24 07:07:28 +0200

    Task 23.3 - Sending emails : CC

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,21 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Mail {
+
+    private String mailTo;
+    private String mailCc;
+    private String subject;
+    private String message;
+
+    public Mail(String mailTo, String subject, String message) {
+        this.mailTo = mailTo;
+        this.subject = subject;
+        this.message = message;
+    }
+
+}
