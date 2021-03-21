86933c6936a0624d7de27599a70a4db2de86c057
michal-tokarski <michal_tokarski@wp.pl>
2021-03-21 17:31:29 +0100

    Module 32.2 - Adding variables to mail template

---
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -0,0 +1,38 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class MailCreatorService {
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
+    @Qualifier("templateEngine")
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+
+    public String buildTrelloCardEmail(String message) {
+
+        Context context = new Context();
+
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8080/tasks_frontend");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+
+        return templateEngine.process("mail/created-trello-card-mail", context);
+
+    }
+
+}
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class SimpleEmailService {
@@ -17,30 +21,48 @@ public class SimpleEmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private MailCreatorService mailCreatorService;
+
     public void send(final Mail mail) {
 
         LOGGER.info("Starting email preparation ...");
 
         try {
             SimpleMailMessage mailMessage = createMailMessage(mail);
-            javaMailSender.send(mailMessage);
+            // javaMailSender.send(mailMessage);
+            javaMailSender.send(createMailMessage(mail));
             LOGGER.info("Email has been sent.");
         } catch (MailException e) {
             LOGGER.error("Failed  to process email sending: [{}] ", e.getMessage(), e);
         }
     }
 
-        private SimpleMailMessage createMailMessage (final Mail mail){
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
 
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setTo(mail.getMailTo());
-            mailMessage.setSubject(mail.getSubject());
-            mailMessage.setText(mail.getMessage());
-            if (mail.getMailCc()!=null && !mail.getMailCc().equals("")) {
-                mailMessage.setCc(mail.getMailCc());
-            }
-            return mailMessage;
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+
+    }
+
+    private SimpleMailMessage createMailMessage (final Mail mail){
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        // mailMessage.setText(mail.getMessage()); // old line
+        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage())); // new line
+        if (mail.getMailCc()!=null && !mail.getMailCc().equals("")) {
+            mailMessage.setCc(mail.getMailCc());
         }
+        return mailMessage;
+
+    }
+
 
 }
 
